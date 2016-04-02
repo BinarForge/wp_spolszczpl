a0cb4683cff4934aa15d72eca8bbddcfff010317
evig <bartlomiej.karalus@gmail.com>
2016-04-03 00:57:49 +0100

    Created root plugin file with basic information. It will now appear in wordpress dashboard on a plugins list.

---
+++ wp_spolszczpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package wp_spolszczpl
+ * @version 0.1
+ */
+/*
+Plugin Name: SpolszczPL
+Plugin URI: http://kuzniabinarna.pl
+Description: Plugin consumes spolszcz.pl API to correct content with polish special characters.
+Author: Bart Karalus
+Version: 1.0
+Author URI: http://kuzniabinarna.pl
+*/
