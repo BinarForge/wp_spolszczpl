3f076b8c316b297b17cd12884416238d596a37ec
evig <bartlomiej.karalus@gmail.com>
2016-04-03 20:00:56 +0100

    When user is editing a post in the dashboard, it now displays a dummy button to transform post content.

--- wp_spolszczpl.php
+++ wp_spolszczpl.php
@@ -1,4 +1,7 @@
 <?php
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
 /**
  * @package wp_spolszczpl
  * @version 0.1
@@ -11,3 +14,16 @@ Author: Bart Karalus
 Version: 1.0
 Author URI: http://kuzniabinarna.pl
 */
+
+
+add_action( 'add_meta_boxes', 'on_add_meta_boxes' );
+function on_add_meta_boxes(){
+    add_meta_box('spolszcz_pl', 'Polskie Znaki', 'wp_spolszczpl_meta', 'post', 'side');
+}
+
+function wp_spolszczpl_meta( $post ) {
+    ?>
+    <button class="button">Dodaj polskie znaki</button>
+    <div class="clear"></div>
+    <?php
+}
