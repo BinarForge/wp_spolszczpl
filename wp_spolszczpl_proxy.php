92f959a42fa57694083f2f54ba7211779f99c721
binarforge <contact@binarforge.com>
2016-05-07 20:39:59 +0100

    Added some safety check for non-ajax requests to the plugin's proxy.

--- wp_spolszczpl_proxy.php
+++ wp_spolszczpl_proxy.php
@@ -1,10 +1,12 @@
 <?php
 
+if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+	die;
+}
+
 if(isset($_POST['txt']))
 {
 	$SC = new SoapClient("http://www.spolszcz.pl/webapi.wsdl");
 	echo $SC->Spolszcz($_POST['txt']);
 	exit;
 }
-
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
